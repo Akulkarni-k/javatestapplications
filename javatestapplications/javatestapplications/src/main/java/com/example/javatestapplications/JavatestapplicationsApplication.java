3c0b66394aec8ce405097b079a3763d30478e06f
Akulkarni-k <65461062+Akulkarni-k@users.noreply.github.com>
2024-03-25 21:56:43 +0530

    Add files via upload

---
+++ javatestapplications/javatestapplications/src/main/java/com/example/javatestapplications/JavatestapplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatestapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatestapplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatestapplicationsApplication.class, args);
+	}
+
+}
